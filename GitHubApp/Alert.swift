bdb2d709e61cc7ccf9b6e4016e4cf819010440b7
Konstantins Belcickis <belchitsky@rochev.studio>
2020-06-18 11:10:46 +0200

    Finish lesson4

---
+++ GitHubApp/Alert.swift
@@ -0,0 +1,9 @@
+//
+//  Alert.swift
+//  GitHubApp
+//
+//  Created by Konstantins Belcickis on 17/06/2020.
+//  Copyright © 2020 Konstantins Belcickis. All rights reserved.
+//
+
+import Foundation
---
+++ GitHubApp/ViewControllers/WebViewController.swift
@@ -0,0 +1,47 @@
+//
+//  WKwebViewController.swift
+//  GitHubApp
+//
+//  Created by Konstantins Belcickis on 16/06/2020.
+//  Copyright © 2020 Konstantins Belcickis. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import WebKit
+import SafariServices
+
+class WebViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    var urlSting: String!
+    let myDataDetector: WKDataDetectorTypes = []
+    
+    override func loadView() {
+        
+        let source = "document.body.style.background = \"#addfad\";"
+        let userScript = WKUserScript(source: source, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+        
+        let userContentController = WKUserContentController()
+        userContentController.addUserScript(userScript)
+        
+        let webConfiguration = WKWebViewConfiguration()
+       webConfiguration.userContentController = userContentController
+        
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        webView.navigationDelegate = self
+        
+        view = webView
+        webView.reload()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let myURL = URL(string: urlSting)
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+        webView.allowsBackForwardNavigationGestures = true
+    }
+}
+
